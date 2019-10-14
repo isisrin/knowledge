54f87c824cd879f1a013ee16b38e118da70efa46
isisrin <isisrin@nate.com>
2019-10-14 22:04:09 +0900

    술집이름은 상수가 맞겠고, 메뉴는 읽기전용이 맞는 것 같아 수정함

--- language/kotlin/sandbox/src/Hello.kt
+++ language/kotlin/sandbox/src/Hello.kt
@@ -1,12 +1,12 @@
 const val MAX_EXPERIENCE: Int = 5000
+const val VAR_NAME = "Unicorn's Horn"
 fun main(args: Array<String>) {
     val playerName = "Hyerin"
     var experiencePoints = 5
     var hasSteed = false
-    var varName = "Unicorn's Horn"
     var hasHorse = false
     var money = 50
-    var menu = mapOf("beer" to 3, "soju" to 5)
+    val menu = mapOf("beer" to 3, "soju" to 5)
     experiencePoints += 5
     println(experiencePoints)
     println(playerName)
