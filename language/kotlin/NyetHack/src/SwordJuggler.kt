a1faf040962c053870ae8ffbcc0e927dde53d3b5
isisrin <isisrin@nate.com>
2019-10-20 19:38:34 +0900

    6장 null처리  - 익셉션 처리 끝!

--- language/kotlin/NyetHack/src/SwordJuggler.kt
+++ language/kotlin/NyetHack/src/SwordJuggler.kt
@@ -7,12 +7,18 @@ fun main(args: Array<String>) {
         swordsJuggling = 2
     }
 
-    proficiencyCheck(swordsJuggling)
-    swordsJuggling = swordsJuggling!!.plus(1)
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
 
     println("$swordsJuggling 개의 칼로 저글링 스타뚜 ")
 }
 
 fun proficiencyCheck(swordsJuggling: Int?) {
-    swordsJuggling ?: throw IllegalArgumentException("저글링이 불가눙햅")
+    checkNotNull(swordsJuggling, {"저글링이 불가눙햅"})
 }
+
+class UnskilledSwordJugglerException : IllegalArgumentException("저글링이 불가눙햅")
